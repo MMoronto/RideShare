41e0e708f1164aaffe49979c9317ca26a0d08bde
MMORONTO <t2wycemo@gmail.com>
2022-03-04 20:47:32 -0600

    update image url for navOption buttons

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -23,14 +23,14 @@ const NavOptions = () => {
         horizontal
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-            <TouchableOpacity style={`twp-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-4`}>
+            <TouchableOpacity style={`twp-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
                 <View>
                     <Image 
                         style={{ width: 120, height: 120, resizeMode: "contain" }}
                         source={{ uri: item.image }}
                     />
                 </View>
-                {/* <Text>{item.title}</Text> */}
+                <Text>{item.title}</Text>
             </TouchableOpacity>
         )}
     />
