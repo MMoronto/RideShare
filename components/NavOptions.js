2fdc5111e6a6ae1948a6f9bae9b190dd36463048
MMORONTO <t2wycemo@gmail.com>
2022-03-08 19:02:20 -0600

    fix render error-debug

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 
 const data = [
