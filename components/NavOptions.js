99d0de4e631e68779505597cc1aca4b36dd65cb3
MMORONTO <t2wycemo@gmail.com>
2022-04-04 12:21:40 -0500

    import touchable opacity into navOptions. fix render bug issue

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,12 +1,33 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { TouchableOpacity, FlatList, Text, View } from 'react-native';
+
+const data = [
+    {
+        id: "123",
+        title: "Hire a hauler",
+        image: "https://links.papareact.com/3pn",
+        screen: "MapScreen",
+    },
+    {
+        id: "456",
+        title: "Order item",
+        image: "https://links.papareact.com/2Bw",
+        screen: "ShopsScreen", // Revise later       
+    },
+];
 
 const NavOptions = () => {
   return (
-    <View>
-      <Text>Hello Motto</Text>
-    </View>
-  )
-}
+    <FlatList 
+        data={data}
+        horizontal
+        renderItem={({ item }) => (
+            <TouchableOpacity>
+                <Text>{item.title}</Text>
+            </TouchableOpacity>
+        )}
+    />
+  );
+};
 
-export default NavOptions
+export default NavOptions;
