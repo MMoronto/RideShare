b28fbc3984c30040856420c08c301d7b9004cd99
MMORONTO <t2wycemo@gmail.com>
2022-04-26 15:55:55 -0500

    import useDispatch

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,9 +4,10 @@ import tw from 'tailwind-react-native-classnames';
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
 
 const HomeScreen = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   return (
     <SafeAreaView style={tw`bg-gray-100 h-full`}>
