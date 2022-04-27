fcc2e4be9b62c40f4f7beb66dc6dd8a4a1cb93a0
MMORONTO <t2wycemo@gmail.com>
2022-04-27 12:44:39 -0500

    import tailwind.css

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import tw from "tailwind-react-native-classnames";
 
 const Map = () => {
   return (
