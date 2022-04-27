5c6184ce1b0aa16b1e434d4c0a8dc3e386e216e9
MMORONTO <t2wycemo@gmail.com>
2022-04-27 12:42:23 -0500

    import mapview marker, style mapview component

--- components/Map.js
+++ components/Map.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 const Map = () => {
   return (
     <MapView
+    style={tw`flex-1`}
         initialRegion={{
         latitude: 37.78825,
         longitude: -122.4324,
