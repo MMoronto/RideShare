ee2802e23ec8849b9eaa7c79aec200b6cc141722
MMORONTO <t2wycemo@gmail.com>
2022-05-01 18:37:41 -0500

    create HaulOptionsCard.js

---
+++ components/HaulOptionsCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HaulOptionsCard = () => {
+  return (
+    <View>
+      <Text>HaulOptionsCard</Text>
+    </View>
+  )
+}
+
+export default HaulOptionsCard
+
+const styles = StyleSheet.create({})
