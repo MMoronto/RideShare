e36e0bffa1e1887f3a3a01558f5f97191b4084a1
MMORONTO <t2wycemo@gmail.com>
2022-05-02 12:20:23 -0500

    Update / import SafeAreaView into navigateCard.js

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 const NavigateCard = () => {
   return (
