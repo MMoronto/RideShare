a50c9f811d64967e0560fb205d4576f572272898
MMORONTO <t2wycemo@gmail.com>
2022-05-03 12:31:20 -0500

    style safe area border

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -6,9 +6,10 @@ const NavigateCard = () => {
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}>NavigateCard</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}></View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default NavigateCard;
 
