84f58142ea410e3998e7daa39d4feae502fb6608
MMORONTO <t2wycemo@gmail.com>
2022-05-19 17:17:28 -0500

    style markers. define  padding dimensions

--- components/Map.js
+++ components/Map.js
@@ -17,7 +17,9 @@ const Map = () => {
         if (!origin || !destination) return;
 
         // Zoom & fit to markers
-        mapRef.current.fitToSuppliedMarkers(['origin', 'destination'])        
+        mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+            edgePadding: { top: 50, right: 50, bottom: 50, left: 50 }
+        });        
     }, [origin, destination])
 
     return (
