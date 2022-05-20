6c8cf6697e52c39243f2ef9078dceb1b2718cce6
MMORONTO <t2wycemo@gmail.com>
2022-05-20 10:19:24 -0500

    build navFavorites component

--- components/NavFavorites.js
+++ components/NavFavorites.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-const NavFavorites = () => {
+const data = [
+    {
+        id: "123",
+        icon: "home",
+        location: "Home",
+        destination: "Allen Avenue, Lagos, Nigeria",
+    },
+    {
+        id: "456",
+        icon: "portmanteau",
+        location: "Home",
+        destination: "Opebi Rd, Lagos, Nigeria",
+    },
+];
 
-    const data = [
-        {
-            id: "123",
-            icon: "home",
-            location: "Home",
-            destination: "Allen Avenue, Lagos, Nigeria",
-        },
-        {
-            id: "456",
-            icon: "portmanteau",
-            location: "Home",
-            destination: "Opebi Rd, Lagos, Nigeria",
-        },
-    ];
+const NavFavorites = () => {
   return (
     <View>
       <Text>NavFavorites</Text>
