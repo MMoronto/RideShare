2ab3ef62ffbb0558fb39635ae84d86ec2cd3cecd
MMORONTO <t2wycemo@gmail.com>
2022-05-19 21:08:49 -0500

    populate flat list with data

--- components/NavFavorites.js
+++ components/NavFavorites.js
@@ -1,7 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 const NavFavorites = () => {
+
+    const data = [
+        {
+            id: "123",
+            icon: "home",
+            location: "Home",
+            destination: "Allen Avenue, Lagos, Nigeria",
+        },
+        {
+            id: "456",
+            icon: "portmanteau",
+            location: "Home",
+            destination: "Opebi Rd, Lagos, Nigeria",
+        },
+    ]:
   return (
     <View>
       <Text>NavFavorites</Text>
