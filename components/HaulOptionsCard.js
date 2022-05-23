14c207ea93e9213a1a816ab5bd4583f3732b43f4
MMORONTO <t2wycemo@gmail.com>
2022-05-23 12:02:03 -0500

    implement HaulOptionsCard nav controls

--- components/HaulOptionsCard.js
+++ components/HaulOptionsCard.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 const HaulOptionsCard = () => {
+  const navigation = useNavigation();
   return (
     <SafeAreaView Style={tw`bg-white flex-grow`}>
       <View>
-        <TouchableOpacity style={tw`absolute top-3 left-5 p-3 rounded-full`}>
+        <TouchableOpacity 
+          onPress={() => navigation.navigate("NavigateCard")} 
+          style={tw`absolute top-3 left-5 p-3 rounded-full`}
+        >
           <Icon name="chevron-left" type="fontawesome" /> 
         </TouchableOpacity>
         <Text Style={tw`text-center py-5 text-xl`}>Hire a hauler</Text>
