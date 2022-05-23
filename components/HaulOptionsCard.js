3070db55f04906a65b1fe107cd976959c4ad1560
MMORONTO <t2wycemo@gmail.com>
2022-05-23 11:45:31 -0500

    import Icon, touchableOpacity components

--- components/HaulOptionsCard.js
+++ components/HaulOptionsCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
+import { Icon } from 'react-native-elements';
 
 const HaulOptionsCard = () => {
   return (
     <SafeAreaView Style={tw`bg-white flex-grow`}>
       <View>
+        <TouchableOpacity>
+
+        </TouchableOpacity>
         <Text Style={tw`text-center py-5 text-xl`}>Hire a hauler</Text>
       </View>
     </SafeAreaView>
