46d749df919281d3b41a9b7475a8e19a85fc93d6
MMORONTO <t2wycemo@gmail.com>
2022-06-01 09:36:43 -0500

    build menu button

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import tw from "tailwind-react-native-classnames";
 import Map from '../components/Map';
 import NavigateCard from '../components/NavigateCard';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StackActions } from '@react-navigation/native-stack';
 import HaulOptionsCard from '../components/HaulOptionsCard';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Icon } from 'react-native-elements/dist/icons/Icon';
 
 const MapScreen = () => {
   const NativeStack = createNativeStackNavigator();
   return (
     <View>
+
+      <TouchableOpacity style={tw`absolute top-16 left-8`}>
+        <Icon name="menu" />
+      </TouchableOpacity>
+
       <View style={tw`h-1/2`}>
         < Map/>
       </View>
